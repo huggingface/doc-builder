a88ac6891f2871b885ea11b60118c3d46b68ddb7
Lysandre <lysandre.debut@reseau.eseo.fr>
2021-11-24 11:42:11 +0100

    Script launch

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import find_packages, setup
+
+
+install_requires = [
+    "tqdm",
+    "pyyaml",
+]
+
+extras = {}
+
+extras["transformers"] = [
+    "transformers[dev]",
+]
+
+setup(
+    name="doc-builder",
+    version="0.0.1.dev0",
+    author="Hugging Face, Inc.",
+    author_email="sylvain@huggingface.co",
+    description="Doc building utility",
+    long_description=open("README.md", "r", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    keywords="doc documentation doc-builder huggingface hugging face",
+    url="https://github.com/huggingface/doc-builder",
+    package_dir={"": "src"},
+    packages=find_packages("src"),
+    extras_require=extras,
+    install_requires=install_requires,
+)
--- src/test_with_transformers.py
+++
@@ -1,5 +0,0 @@
-from doc_builder import build_doc
-import os
-
-if __name__ == "__main__":
-    build_doc("transformers", "../transformers/docs/source/", "build")
