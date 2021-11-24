874b3ffdf247592aa7ecb0eaebda13e8ad742f4a
Lysandre <lysandre.debut@reseau.eseo.fr>
2021-11-24 09:47:38 +0100

    Update the version management

--- src/build.py
+++ src/build.py
@@ -12,13 +12,27 @@ if __name__ == "__main__":
              "documentation files should be indicated here."
     )
     parser.add_argument("--language", type=str, help="Language of the documentation to generate", default="en")
+    parser.add_argument(
+        "--version",
+        type=str,
+        help="Version under which to push the files. Will not affect the actual files generated, as these are"
+             " generated according to the `path_to_docs` argument.",
+    )
 
     args = parser.parse_args()
 
-    module = importlib.import_module(args.library_name)
-    version = module.__version__
+    if args.version is None:
+        module = importlib.import_module(args.library_name)
+        version = module.__version__
 
-    output_path = f"./build/{args.library_name}/v{version}/{args.language}"
+        if "dev" in version:
+            version = "master"
+        else:
+            version = "v" + str(version)
+    else:
+        version = args.version
+
+    output_path = f"./build/{args.library_name}/{version}/{args.language}"
 
     print("Building docs for", args.library_name, args.path_to_docs, output_path)
     build_doc(args.library_name, args.path_to_docs, output_path)
