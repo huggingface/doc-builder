653ec3a72488074f333521f90b814b18a83c39d8
Mishig Davaadorj <dmishig@gmail.com>
2022-01-13 07:54:43 -0700

    Dev build should not update _versions.yml file (#75)
    * Update build.py
    
    * Update build.py
    
    * Chore

--- src/doc_builder/commands/build.py
+++ src/doc_builder/commands/build.py
@@ -46,8 +46,9 @@ def build_command(args):
         notebook_dir=args.notebook_dir,
     )
 
+    # dev build should not update _versions.yml
     package_doc_path = os.path.join(args.build_dir, args.library_name)
-    if os.path.isfile(os.path.join(package_doc_path, "_versions.yml")):
+    if "pr_" not in version and os.path.isfile(os.path.join(package_doc_path, "_versions.yml")):
         update_versions_file(os.path.join(args.build_dir, args.library_name), version)
 
 
