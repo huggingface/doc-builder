7f68f070e3fe06363ed5451aca1184cfe8d4ccc9
Mishig Davaadorj <dmishig@gmail.com>
2022-01-31 21:38:33 +0100

    Test inserting dupkicate value into versions.yml

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -34,7 +34,7 @@ class UtilsTester(unittest.TestCase):
                 expected_yml = "- version: master\n- version: v4.2.3\n- version: v4.2.2\n- version: v4.2.1\n"
                 self.assertEqual(yml_str, expected_yml)
 
-        # test ynl with master version only
+        # test yml with master version only
         with tempfile.TemporaryDirectory() as tmp_dir:
             with open(f"{tmp_dir}/_versions.yml", "w") as tmp_yml:
                 versions = [{"version": "master"}]
@@ -52,3 +52,15 @@ class UtilsTester(unittest.TestCase):
                 yaml.dump(versions, tmp_yml)
 
             self.assertRaises(ValueError, update_versions_file, tmp_dir, "v4.2.2")
+
+        # test inserting duplicate value into yml
+        with tempfile.TemporaryDirectory() as tmp_dir:
+            with open(f"{tmp_dir}/_versions.yml", "w") as tmp_yml:
+                versions = [{"version": "master"}]
+                yaml.dump(versions, tmp_yml)
+            update_versions_file(tmp_dir, "v4.2.2")
+            update_versions_file(tmp_dir, "v4.2.2")  # inserting duplicate value
+            with open(f"{tmp_dir}/_versions.yml", "r") as tmp_yml:
+                yml_str = tmp_yml.read()
+                expected_yml = "- version: master\n- version: v4.2.2\n"
+                self.assertEqual(yml_str, expected_yml)
