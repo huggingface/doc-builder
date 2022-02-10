43d9c58ecaaadc3d81fd4295e0a762b01835d44b
Mishig Davaadorj <dmishig@gmail.com>
2022-02-10 09:57:04 +0100

    Convert numpy doc style to groups doc style

--- src/doc_builder/utils.py
+++ src/doc_builder/utils.py
@@ -16,8 +16,10 @@
 import importlib.machinery
 import importlib.util
 import os
+import re
 
 import yaml
+from numpydoc.docscrape import NumpyDocString
 from packaging import version as package_version
 
 
@@ -74,3 +76,76 @@ def get_doc_config():
     Returns the `doc_config` if it has been loaded.
     """
     return doc_config
+
+
+def convert_parametype_numpydoc_to_groupsdoc(paramtype):
+    """
+    Converts parameter type str that is written in numpystyle to groupsstyle (e.g. transformers docs).
+
+    Args:
+    - **paramtype** (`str`) -- The name of the object to document.
+
+    Example::
+
+        >>> s = "bool, optional, default True"
+        >>> convert_numpydoc_to_rstdoc(s)
+        '`bool`, `optional`, defults to `True`'
+    """
+    paramtype = re.sub(r"\s*\(.*\)$", "", paramtype).replace("default", "defults to")
+    arr = paramtype.split(", ")
+    for i, s in enumerate(arr):
+        s = s.split(" ")
+        s[-1] = f"`{s[-1]}`"
+        arr[i] = " ".join(s)
+    return ", ".join(arr)
+
+
+def convert_numpydoc_to_groupsdoc(docstring):
+    """
+    Converts docstring that is written in numpystyle to groupsstyle (e.g. transformers docs).
+
+    Args:
+    - **docstring** (`str`) -- Docstring that is written in numpystyle.
+    """
+    numydoc = NumpyDocString(docstring)
+
+    def helper(arr):
+        """
+        Helper function that joins array into space-separated string.
+        """
+        arr = [a.strip() for a in arr if a.strip()]
+        if arr:
+            return " ".join(arr) + "\n"
+        return ""
+
+    indent = "    "
+    result = ""
+    result += helper(numydoc["Summary"])
+    if numydoc["Parameters"]:
+        result += "\nArgs:\n"
+        for parameter in numydoc["Parameters"]:
+            result += indent + parameter[0]
+            if parameter[1]:
+                result += f" ({convert_parametype_numpydoc_to_groupsdoc(parameter[1])})"
+            result += ":\n"
+            if parameter[2]:
+                result += indent * 2 + helper(parameter[2])
+    if numydoc["Returns"]:
+        result += "\nReturns:\n"
+        for parameter in numydoc["Returns"]:
+            returnttype = f":obj:'{parameter[1]}'" if parameter[1] else ""
+            result += indent + ": ".join(s for s in [returnttype, helper(parameter[2])] if s)
+    if numydoc["Raises"]:
+        result += "\nRaises:\n"
+        for parameter in numydoc["Raises"]:
+            raiseerror = f"{parameter[1]}" if parameter[1] else ""
+            result += indent + ": ".join(s for s in [raiseerror, helper(parameter[2])] if s)
+    if numydoc["Examples"]:
+        result += "\nExample::\n"
+        first_line = numydoc["Examples"][0]
+        indent_n = len(first_line) - len(first_line.lstrip())
+        examples = [indent + line[indent_n:] for line in numydoc["Examples"]]
+        examples_str = "\n".join(examples)
+        result += examples_str
+
+    return result
