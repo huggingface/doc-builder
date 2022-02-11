22948c50df0305c29b56cb29ace5bfdb0f2fb4b3
Mishig Davaadorj <dmishig@gmail.com>
2022-02-11 09:51:15 +0100

    Update src/doc_builder/utils.py
    Co-authored-by: Sylvain Gugger <35901082+sgugger@users.noreply.github.com>

--- src/doc_builder/utils.py
+++ src/doc_builder/utils.py
@@ -141,7 +141,7 @@ def convert_numpydoc_to_groupsdoc(docstring):
     if numydoc["Returns"]:
         result = start_section(result, "Returns")
         for parameter in numydoc["Returns"]:
-            returnttype = f":obj:'{parameter[1]}'" if parameter[1] else ""
+            returnttype = f":obj:`{parameter[1]}`" if parameter[1] else ""
             result += indent + ": ".join(s for s in [returnttype, stringify_arr(parameter[2])] if s)
     if numydoc["Raises"]:
         result = start_section(result, "Raises")
