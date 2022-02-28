e0c1f79c163907cb6fcf1fa8460de649d04672cb
Mishig Davaadorj <dmishig@gmail.com>
2022-03-01 00:28:16 +0100

    Make generated CSS empty

--- kit/postbuild.sh
+++ kit/postbuild.sh
@@ -3,3 +3,7 @@ set -euo pipefail
 IFS=$'\n\t'
 
 cp src/routes/_toctree.yml build/_toctree.yml
+
+# To void conflict with Hub Tailwind CSS build, 
+# making doc-builder's PostCSS geenrated file an empty one
+> $(find . -regex '.*build.*.css')
