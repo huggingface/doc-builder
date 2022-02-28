0f6dd86b2b45d1f63f87b912d00354573bb567d4
Mishig Davaadorj <dmishig@gmail.com>
2022-03-01 00:35:13 +0100

    Typo

--- kit/postbuild.sh
+++ kit/postbuild.sh
@@ -4,6 +4,6 @@ IFS=$'\n\t'
 
 cp src/routes/_toctree.yml build/_toctree.yml
 
-# To void conflict with Hub Tailwind CSS build, 
+# To avoid conflict with Hub Tailwind CSS build, 
 # making doc-builder's PostCSS geenrated file an empty one
 > $(find . -regex '.*build.*.css')
