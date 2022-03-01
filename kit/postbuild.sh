005fcf7fd4f3bffde32ebf411d5f3fb17b89cc79
Mishig Davaadorj <dmishig@gmail.com>
2022-03-01 10:26:12 +0100

    Make stylesheet `modulepreload`

--- kit/postbuild.sh
+++ kit/postbuild.sh
@@ -5,5 +5,8 @@ IFS=$'\n\t'
 cp src/routes/_toctree.yml build/_toctree.yml
 
 # To avoid conflict with Hub Tailwind CSS build, 
-# making doc-builder's PostCSS geenrated file an empty one
+# 1. making doc-builder's PostCSS geenrated file an empty one
+# 2. making `rel="stylesheet"` -> `rel="modulepreload"`
 > $(find . -regex '.*build.*.css')
+cd build
+find . -name '*.html' -exec sed -i '' 's/rel="stylesheet"/rel="modulepreload"/g' {} \;
