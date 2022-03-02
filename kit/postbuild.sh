a32fe96ff9db100cdae6497b45434b25554a800a
Mishig Davaadorj <dmishig@gmail.com>
2022-03-02 10:30:41 +0100

    Make generated CSS empty (#115)
    * Make generated CSS empty
    
    * Typo
    
    * Make stylesheet `modulepreload`

--- kit/postbuild.sh
+++ kit/postbuild.sh
@@ -3,3 +3,10 @@ set -euo pipefail
 IFS=$'\n\t'
 
 cp src/routes/_toctree.yml build/_toctree.yml
+
+# To avoid conflict with Hub Tailwind CSS build, 
+# 1. making doc-builder's PostCSS geenrated file an empty one
+# 2. making `rel="stylesheet"` -> `rel="modulepreload"`
+> $(find . -regex '.*build.*.css')
+cd build
+find . -name '*.html' -exec sed -i '' 's/rel="stylesheet"/rel="modulepreload"/g' {} \;
