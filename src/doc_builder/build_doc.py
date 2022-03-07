10af379cd36accdb180839a6778898609be25a71
Sylvain Gugger <Sylvain.gugger@gmail.com>
2022-03-07 14:36:36 -0500

    Remove debug statement

--- src/doc_builder/build_doc.py
+++ src/doc_builder/build_doc.py
@@ -444,7 +444,6 @@ def convert_anchors_mapping_to_sphinx_format(anchors_mapping, package):
             obj_type = "py:method" if obj.__name__ != obj.__qualname__ else "py:function"
         else:
             # Default to function
-            print(anchor)
             obj_type = "py:function"
 
         sphinx_refs.append(f"{anchor} {obj_type} 1 {url}#$ -")
