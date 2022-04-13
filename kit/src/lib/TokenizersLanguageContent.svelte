ef1b6babdfafc2dd8f0aebcb7b415c9071259301
Mishig Davaadorj <dmishig@gmail.com>
2022-04-13 17:00:07 +0200

    Fix 2xl:-mx-4 margin

--- kit/src/lib/TokenizersLanguageContent.svelte
+++ kit/src/lib/TokenizersLanguageContent.svelte
@@ -32,7 +32,7 @@
 	export let node = false;
 </script>
 
-<div class="flex space-x-2 items-center my-1.5 mr-8 h-7 !pl-0 -mx-3 md:mx-0 2xl:-mx-4">
+<div class="flex space-x-2 items-center my-1.5 mr-8 h-7 !pl-0 -mx-3 md:mx-0">
 	{#each LANGUAGES_CONFIG as language}
 		<div
 			class="flex items-center border rounded-lg px-1.5 py-1 leading-none select-none text-smd
