658d7e1493c00a3ec7fb8a7a203f8cf2381a3e0e
Mishig Davaadorj <dmishig@gmail.com>
2022-05-18 15:36:41 +0200

    Add `doc-builder commit` cmd

---
+++ src/doc_builder/commands/commit.py
@@ -0,0 +1,144 @@
+# coding=utf-8
+# Copyright 2022 The HuggingFace Team. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import argparse
+import base64
+import re
+from pathlib import Path
+
+import requests
+from gql import Client, gql
+from gql.transport.aiohttp import AIOHTTPTransport
+from gql.transport.exceptions import TransportQueryError
+
+
+_re_head_oid = re.compile(r"\"sha\":\"([0-9a-z]+)\"")
+
+
+def get_head_oid(repo_id, token, branch="main"):
+    """
+    Returns last commit sha from repostory `repo_id` & branch `branch`
+    """
+    res = requests.get(
+        f"https://api.github.com/repos/{repo_id}/git/refs/heads/${branch}",
+        headers={"Authorization": f"bearer {token}"},
+    )
+    if res.status_code == 200:
+        head_oid = _re_head_oid.search(res.text)[1]
+        return head_oid
+    else:
+        raise Exception("get_head_oid failed: {{status: {res.status}, msg:{res.text}}}")
+
+
+def create_additions_str(path_to_built_docs):
+    """
+    Given `path_to_built_docs` dir, returns a str that is in format: `{path: "some_path", contents: "base64_repr_contents"}, ...`
+    see more here: https://docs.github.com/en/graphql/reference/input-objects#filechanges
+    """
+    p = Path(path_to_built_docs)
+    files = [x for x in p.glob("**/*") if x.is_file()]
+    additions = []
+
+    for fpath in files:
+        with open(fpath, "rb") as reader:
+            content = reader.read()
+            content_base64 = base64.b64encode(content)
+            content_base64 = str(content_base64, "utf-8")
+            additions.append(f'{{path: "{fpath}", contents: "{content_base64}"}}')
+
+    additions_str = ", ".join(additions)
+    return additions_str
+
+
+def commit_additions(additions_str, repo_id, head_oid, token, commit_msg):
+    """
+    Commits additions to a repository using Github GraphQL mutation `createCommitOnBranch`
+    see more here: https://docs.github.com/en/graphql/reference/mutations#createcommitonbranch
+    """
+    # Select your transport with a defined url endpoint
+    transport = AIOHTTPTransport(url="https://api.github.com/graphql", headers={"Authorization": f"bearer {token}"})
+    # Create a GraphQL client using the defined transport
+    client = Client(transport=transport, fetch_schema_from_transport=True)
+    # Provide a GraphQL query
+    query = gql(
+        f"""
+  mutation{{
+    createCommitOnBranch(
+      input: {{
+        branch:{{
+          repositoryNameWithOwner: "{repo_id}",
+          branchName: "main"
+        }},
+        message: {{
+          headline: "{commit_msg}"
+        }},
+        fileChanges: {{
+          additions: [{additions_str}]
+        }},
+        expectedHeadOid: "{head_oid}"
+      }}
+    ) {{
+      clientMutationId
+    }}
+  }}
+  """
+    )
+    # Execute the query on the transport
+    result = client.execute(query)
+    return result
+
+
+def commit_command(args):
+    """
+    Commit file additions using Github GraphQL rather than `git`.
+    Usage: doc-builder commit $args
+    """
+    number_of_retries = 5
+    additions_str = create_additions_str(args.path_to_built_docs)
+    while number_of_retries:
+        try:
+            head_oid = get_head_oid(args.doc_build_repo_id, args.token)
+            commit_additions(additions_str, args.doc_build_repo_id, head_oid, args.token, args.commit_msg)
+        except TransportQueryError as e:
+            error_msg = str(e)
+            if "Expected branch to point to" in error_msg:
+                number_of_retries -= 1
+                print(f"Failed on try #{6-number_of_retries}, pushing again")
+
+
+def commit_command_parser(subparsers=None):
+    if subparsers is not None:
+        parser = subparsers.add_parser("commit")
+    else:
+        parser = argparse.ArgumentParser("Doc Builder commit command")
+
+    parser.add_argument(
+        "path_to_built_docs",
+        type=str,
+        help="Local path to library documentation. The library should be cloned, and the folder containing the "
+        "documentation files should be indicated here.",
+    )
+    parser.add_argument("--doc_build_repo_id", type=str, help="Language of the documentation to generate")
+    parser.add_argument("--token", type=str, help="Language of the documentation to generate")
+    parser.add_argument(
+        "--commit_msg",
+        type=str,
+        help="Version of the documentation to generate",
+        default="Github GraphQL createcommitonbranch commit",
+    )
+
+    if subparsers is not None:
+        parser.set_defaults(func=commit_command)
+    return parser
--- src/doc_builder/commands/doc_builder_cli.py
+++ src/doc_builder/commands/doc_builder_cli.py
@@ -17,6 +17,7 @@
 from argparse import ArgumentParser
 
 from doc_builder.commands.build import build_command_parser
+from doc_builder.commands.commit import commit_command_parser
 from doc_builder.commands.convert_doc_file import convert_command_parser
 from doc_builder.commands.preview import preview_command_parser
 from doc_builder.commands.style import style_command_parser
@@ -31,6 +32,7 @@ def main():
     build_command_parser(subparsers=subparsers)
     style_command_parser(subparsers=subparsers)
     preview_command_parser(subparsers=subparsers)
+    commit_command_parser(subparsers=subparsers)
 
     # Let's go
     args = parser.parse_args()
