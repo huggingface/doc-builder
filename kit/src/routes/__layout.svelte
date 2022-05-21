b36e29e5e80679631bbbeda76c75141c248d9bec
Mishig Davaadorj <dmishig@gmail.com>
2022-05-21 14:46:01 +0100

    Fix preview cmd (#216)
    * Gebearlize toc for preview cmd
    
    * More css
    
    * Better style
    
    * Chore

--- kit/src/routes/__layout.svelte
+++ kit/src/routes/__layout.svelte
@@ -18,15 +18,11 @@
 
 <script lang="ts">
 	import type { LoadInput } from "@sveltejs/kit";
+	import type { RawChapter } from "./endpoints/toc";
 	import { prerendering } from "$app/env";
 	import "../app.css";
 
-	export let toc: Array<{
-		sections: Array<
-			{ title: string } & ({ local: string } | { sections: { title: string; local: string }[] })
-		>;
-		title: string;
-	}>;
+	export let toc: RawChapter[];
 </script>
 
 {#if prerendering || !import.meta.env.DEV}
@@ -40,50 +36,27 @@
 		}
 	</style>
 	<div
-		style="width: 100vh; height: 100vh; margin: 0; padding: 0; display: flex; flex-direction: row"
+		class="flex space-x-4"
 	>
-		<aside
-			style="width: 270px; min-width: 270px; max-width: 270px; border-right: 1px solid gray; height: 100vh; position: fixed; overflow-y: auto; display: flex; flex-direction: column"
-		>
+		<div class="border-r-2">
 			<ul class="pt-2 flex flex-col pl-3">
 				{#each toc as section}
-					<h3
-						class="flex group-hover:after:content-['▶'] after:absolute after:right-4 after:text-gray-500 after:transition after:duration-100 after:ease-in after:transform after:rotate-90"
-					>
-						{section.title}
-					</h3>
-					<div class="pl-4">
-						{#each section.sections as subsection}
-							{#if "sections" in subsection}
-								<h3
-									class="flex group-hover:after:content-['▶'] after:absolute after:right-4 after:text-gray-500 after:transition after:duration-100 after:ease-in after:transform after:rotate-90"
-								>
-									{subsection.title}
-								</h3>
-
-								<ul class="pt-2 flex flex-col pl-3">
-									{#each subsection.sections as finalsection}
-										<a
-											role="navigation"
-											class="block text-gray-500 pr-2 hover:text-black dark:hover:text-gray-300 py-1 transform transition-all hover:translate-x-px first:mt-1 last:mb-4 pl-2 ml-2"
-											href="{base}/{finalsection.local.replace(/\bindex$/, '')}"
-											>{finalsection.title}</a
-										>
-									{/each}
-								</ul>
-							{:else}
-								<a
-									role="navigation"
-									class="block text-gray-500 pr-2 hover:text-black dark:hover:text-gray-300 py-1 transform transition-all hover:translate-x-px first:mt-1 last:mb-4 pl-2 ml-2"
-									href="{base}/{subsection.local.replace(/\bindex$/, '')}">{subsection.title}</a
-								>
-							{/if}
-						{/each}
-					</div>
+					{#if section.local}
+						<a
+							role="navigation"
+							class="block text-gray-500 pr-2 hover:text-black dark:hover:text-gray-300 py-1"
+							href="{base}/{section.local.replace(/\bindex$/, '')}">{section.title}</a
+						>
+					{:else}
+						<span
+						role="navigation"
+						class="opacity-50 text-lg block text-gray-500 pr-2 hover:text-black dark:hover:text-gray-300 py-1"
+						>{section.title}</span>
+					{/if}
 				{/each}
 			</ul>
-		</aside>
-		<div style="margin-left: 270px;" class="px-4 pt-3">
+		</div>
+		<div class="px-4 pt-3">
 			<div class="prose prose-doc dark:prose-light max-w-4xl mx-auto break-words relative">
 				<slot />
 			</div>
