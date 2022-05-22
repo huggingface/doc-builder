33bb7030f6b75d0a9c148970f01834cdfc0b4e7f
Mishig Davaadorj <dmishig@gmail.com>
2022-05-22 22:09:46 +0200

    Rm hash from doc build artifacts

--- kit/postbuild.sh
+++ kit/postbuild.sh
@@ -10,3 +10,7 @@ cp src/routes/_toctree.yml build/_toctree.yml
 > $(find . -regex '.*build.*.css')
 cd build
 find . -name '*.html' -exec perl -pi -e 's/rel="stylesheet"/rel="modulepreload"/g' {} +
+
+# Remove hash from files
+find . -name '*.html' -exec perl -pi -e 's/-[a-z0-9]{8}\.(js|css)/-hf-doc-builder\.$1/g' {} +
+find . -name '*.js' -o -name '*.css' | perl -pe 'print $_; s/-[a-z0-9]{8}\.(js|css)/-hf-doc-builder\.$1/g' | xargs -n2 mv
