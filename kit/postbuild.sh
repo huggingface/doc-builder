358afe5ad3ec733944b68033785a7c971afdb3a1
Mishig Davaadorj <dmishig@gmail.com>
2022-05-23 11:37:36 +0200

    Replace hash in imports of js, json fils too

--- kit/postbuild.sh
+++ kit/postbuild.sh
@@ -16,6 +16,6 @@ cd ..
 # so that git diff can be smaller since different hashes create new files that dont share git history
 # ex: assets/paths-4b3c6e7e.js -> assets/paths-hf-doc-builder.js
 cd build
-find . -name '*.html' -exec perl -pi -e 's/-[a-z0-9]{8}\.(js|css)/-hf-doc-builder\.$1/g' {} +
+find . -type f -exec perl -pi -e 's/-[a-z0-9]{8}\.(js|css)/-hf-doc-builder\.$1/g' {} +
 find . -name '*.js' -o -name '*.css' | perl -pe 'print $_; s/-[a-z0-9]{8}\.(js|css)/-hf-doc-builder\.$1/g' | xargs -n2 mv
 cd ..
