75ec3b0428c2447ca9731270e4d2a6a32ce16439
Mishig Davaadorj <dmishig@gmail.com>
2022-05-23 09:54:24 +0100

    Rm `[hash]` from doc-build artifcats (#218)
    * Rm hash from doc build artifacts
    
    * Add better comment
    
    * Better cd statements

--- kit/postbuild.sh
+++ kit/postbuild.sh
@@ -10,3 +10,12 @@ cp src/routes/_toctree.yml build/_toctree.yml
 > $(find . -regex '.*build.*.css')
 cd build
 find . -name '*.html' -exec perl -pi -e 's/rel="stylesheet"/rel="modulepreload"/g' {} +
+cd ..
+
+# Replace hash in filenames of build artifcats with substring `hf-doc-builder`
+# so that git diff can be smaller since different hashes create new files that dont share git history
+# ex: assets/paths-4b3c6e7e.js -> assets/paths-hf-doc-builder.js
+cd build
+find . -name '*.html' -exec perl -pi -e 's/-[a-z0-9]{8}\.(js|css)/-hf-doc-builder\.$1/g' {} +
+find . -name '*.js' -o -name '*.css' | perl -pe 'print $_; s/-[a-z0-9]{8}\.(js|css)/-hf-doc-builder\.$1/g' | xargs -n2 mv
+cd ..
