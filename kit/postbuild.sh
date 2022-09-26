934ca9d77543facbc0a6934c7d31cb0153bc1167
Mishig Davaadorj <dmishig@gmail.com>
2022-09-26 14:46:06 +0200

    dev change

--- kit/postbuild.sh
+++ kit/postbuild.sh
@@ -15,7 +15,7 @@ cd ..
 # Replace hash in filenames of build artifcats with substring `hf-doc-builder`
 # so that git diff can be smaller since different hashes create new files that dont share git history
 # ex: assets/paths-4b3c6e7e.js -> assets/paths-hf-doc-builder.js
-cd build
-find . -type f -exec perl -pi -e 's/-[a-z0-9]{8}\.(js|css)/-hf-doc-builder\.$1/g' {} +
-find . -name '*.js' -o -name '*.css' | perl -pe 'print $_; s/-[a-z0-9]{8}\.(js|css)/-hf-doc-builder\.$1/g' | xargs -n2 mv
-cd ..
+# cd build
+# find . -type f -exec perl -pi -e 's/-[a-z0-9]{8}\.(js|css)/-hf-doc-builder\.$1/g' {} +
+# find . -name '*.js' -o -name '*.css' | perl -pe 'print $_; s/-[a-z0-9]{8}\.(js|css)/-hf-doc-builder\.$1/g' | xargs -n2 mv
+# cd ..
