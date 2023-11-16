69624b5c5a9cb9f692057267e0e4324e1c00d560
Mishig <mishig.davaadorj@coloradocollege.edu>
2023-11-16 08:29:14 -0800

    Revert "Expose sveltekit goto function to window (#439)"
    This reverts commit ee4caf7c4b970502eaabda92c8b14601b0118713.

--- tests/test_convert_md_to_mdx.py
+++ tests/test_convert_md_to_mdx.py
@@ -33,7 +33,6 @@ class ConvertMdToMdxTester(unittest.TestCase):
         md_text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit"
         expected_conversion = """<script lang="ts">
 import {onMount} from "svelte";
-import { goto } from '$app/navigation';
 import Tip from "$lib/Tip.svelte";
 import Youtube from "$lib/Youtube.svelte";
 import Docstring from "$lib/Docstring.svelte";
@@ -61,7 +60,6 @@ let fw: "pt" | "tf" = "pt";
 onMount(() => {
     const urlParams = new URLSearchParams(window.location.search);
     fw = urlParams.get("fw") || "pt";
-    window.hfDocGoto = goto;
 });
 </script>
 <svelte:head>
