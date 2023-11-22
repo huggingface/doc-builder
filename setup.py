7bcc3eb7be52c8d07e50870e451ed5424171b5e0
Laureηt <laurentfainsin@protonmail.com>
2023-11-22 10:08:36 +0100

    Update setup.py (#446)
    add `requests` dependency from [/src/doc_builder/external.py#L22](https://github.com/huggingface/doc-builder/blob/c4e348ea1c844d659c65261d914e9cb9237a84e3/src/doc_builder/external.py#L22)

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ install_requires = ["black", "GitPython", "tqdm", "pyyaml", "packaging", "nbform
 extras = {}
 
 extras["transformers"] = ["transformers[dev]"]
-extras["testing"] = ["pytest", "pytest-xdist", "torch", "transformers", "tokenizers", "timm", "google-api-python-client"]
+extras["testing"] = ["pytest", "pytest-xdist", "torch", "transformers", "tokenizers", "timm", "google-api-python-client", "requests"]
 extras["quality"] = ["black~=22.0", "isort>=5.5.4", "flake8>=3.8.3"]
 
 extras["all"] = extras["testing"] + extras["quality"]
