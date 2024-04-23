762a5b3703c8dfb048531ea14d750164413ebcdc
Mishig <dmishig@gmail.com>
2024-04-23 13:58:37 +0200

    Update on Github btn (#498)

---
+++ kit/src/lib/EditOnGithub.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	export let source: string = "";
+</script>
+
+<a
+	class="!text-gray-400 !no-underline text-sm flex items-center not-prose mt-4"
+	href={source}
+	target="_blank"
+>
+	<span>&lt;</span>
+	<span>&gt;</span>
+	<span><span class="underline ml-1.5">Update</span> on GitHub</span>
+</a>
