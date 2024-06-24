4b0d2c61ab8508a5b09573f029f935b1a51c23f6
Mishig <dmishig@gmail.com>
2024-06-24 11:44:42 +0200

    Fix REGEX_INLINE_CODE for escaping # in inline codes (#512)

--- kit/preprocessors/hashInCode.js
+++ kit/preprocessors/hashInCode.js
@@ -5,7 +5,7 @@ import { replaceAsync } from "./utils.js";
 export const hashInCodePreprocess = {
 	markup: async ({ content }) => {
 		const REGEX_CODE_BLOCK = /```.*?```/gs;
-		const REGEX_INLINE_CODE = /`.*?`/gs;
+		const REGEX_INLINE_CODE = /`.*?`/g;
 		content = await replaceAsync(content, REGEX_CODE_BLOCK, async (codeContent) => {
 			return codeContent.replaceAll("#", "&amp;num;");
 		});
