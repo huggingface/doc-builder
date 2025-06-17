e2cddd2c1f3c6bae6da69fdff6929bccb5084d88
Mishig Davaadorj <dmishig@gmail.com>
2025-06-17 16:20:53 +0200

    refactor: make auth tokens optional with default values and reorder searchable attributes

--- src/doc_builder/meilisearch_helper.py
+++ src/doc_builder/meilisearch_helper.py
@@ -58,14 +58,14 @@ def create_embedding_db(client: Client, index_name: str):
     index = client.index(index_name)
     task_info = index.update_embedders({VECTOR_NAME: {"source": "userProvided", "dimensions": VECTOR_DIM}})
     return client, task_info
-
+    
 
 @wait_for_task_completion
 def update_db_settings(client: Client, index_name: str):
     index = client.index(index_name)
     task_info = index.update_settings(
         {
-            "searchableAttributes": ["text", "heading1", "heading2", "heading3", "heading4", "heading5"],
+            "searchableAttributes": ["heading1", "heading2", "heading3", "heading4", "heading5", "text"],
             "filterableAttributes": ["library"],
         }
     )
