282bae3cc726ef022306462e0e933e1899c356cd
Mishig Davaadorj <dmishig@gmail.com>
2025-06-23 15:54:04 +0200

    Update meilisearch dependency to version 0.34.1 in setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 
 from setuptools import find_packages, setup
 
-install_requires = ["black", "GitPython", "tqdm", "pyyaml", "packaging", "nbformat", "huggingface_hub @ git+https://github.com/huggingface/huggingface_hub.git@feature_extraction_working", "pillow", "meilisearch"]
+install_requires = ["black", "GitPython", "tqdm", "pyyaml", "packaging", "nbformat", "huggingface_hub @ git+https://github.com/huggingface/huggingface_hub.git@feature_extraction_working", "pillow", "meilisearch==0.34.1"]
 
 extras = {}
 
